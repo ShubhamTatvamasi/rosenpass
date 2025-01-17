856745f57a5353680abbee5e82d67e8c7941b3ac
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2025-01-17 15:21:41 +0530

    modified:   assign-ip-client.sh

--- start-client.sh
+++ start-client.sh
@@ -1,6 +1,6 @@
 sudo rp exchange \
   client.rosenpass-secret \
-  dev rosenpass0 \
+  dev quantum-vpn0 \
   peer server.rosenpass-public \
   endpoint 192.168.1.152:9999 \
   allowed-ips 10.10.10.0/24
--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,6 @@
 sudo rp exchange \
   server.rosenpass-secret \
-  dev rosenpass0 \
+  dev quantum-vpn0 \
   listen 192.168.1.152:9999 \
   peer client.rosenpass-public \
   allowed-ips 10.10.10.0/24
